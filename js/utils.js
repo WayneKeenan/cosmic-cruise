346f4454b44ae9b9d198638a0120f4e1e685bd05
Wayne Keenan <WayneKeenan@users.noreply.github.com>
2017-04-11 08:14:19 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,10 +3,6 @@ var $buoop = {}
 $buoop.ol = window.onload; 
 window.onload=function(){ 
 	try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
-	var e = document.createElement("script"); 
-	e.setAttribute("type", "text/javascript"); 
-	e.setAttribute("src", "http://browser-update.org/update.js"); 
-	document.body.appendChild(e); 
 }
 
 //make window.requestAnimationFrame cross-browser
